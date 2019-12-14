9f86306d2eca220a12f617c985a5f479a94a44d1
Dmitry Khlystov <dkhlystov@gmail.com>
2019-12-14 15:51:25 +1000

    Refactoring

--- helpers/Translit.php
+++ helpers/Translit.php
@@ -8,81 +8,55 @@ namespace dkhlystov\helpers;
  */
 class Translit
 {
+    /**
+     * @var array replace list. Most friendly with Google and Yandex.
+     */
+    public static $replace = [
+        'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd',
+        'е' => 'e', 'ё' => 'e', 'ж' => 'j', 'з' => 'z', 'и' => 'i',
+        'й' => 'y', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n',
+        'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't',
+        'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'ts', 'ч' => 'ch',
+        'ш' => 'sh', 'щ' => 'shh', 'ъ' => '', 'ы' => 'y', 'ь' => '',
+        'э' => 'e', 'ю' => 'u', 'я' => 'ya',
 
-	/**
-	 * @var array replace list. Most friendly with Google and Yandex.
-	 */
-	private static $_chr = [
+        '-' => '-', ' ' => '-', '.' => '-', ',' => '-',
+    ];
 
-		'0' => '0', '1' => '1', '2' => '2', '3' => '3', '4' => '4',
-		'5' => '5', '6' => '6', '7' => '7', '8' => '8', '9' => '9',
+    /**
+     * Tranlit
+     * @param string $text text for transliteration.
+     * @return string
+     */
+    public static function t($text)
+    {
+        $text = mb_strtolower($text);
 
-		'a' => 'a', 'b' => 'b', 'c' => 'c', 'd' => 'd', 'e' => 'e',
-		'f' => 'f', 'g' => 'g', 'h' => 'h', 'i' => 'i', 'j' => 'j',
-		'k' => 'k', 'l' => 'l', 'm' => 'm', 'n' => 'n', 'o' => 'o',
-		'p' => 'p', 'q' => 'q', 'r' => 'r', 's' => 's', 't' => 't',
-		'u' => 'u', 'v' => 'v', 'w' => 'w', 'x' => 'x', 'y' => 'y',
-		'z' => 'z',
+        // Cyrilic and symbols translit
+        $replace = self::$replace;
+        $s = '';
+        for ($i = 0; $i < mb_strlen($text); $i++) {
+            $c = mb_substr($text, $i, 1);
+            if (array_key_exists($c, $replace)) {
+                $s .= $replace[$c];
+            } else {
+                $s .= $c;
+            }
+        }
 
-		'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd',
-		'е' => 'e', 'ё' => 'e', 'ж' => 'j', 'з' => 'z', 'и' => 'i',
-		'й' => 'y', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n',
-		'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't',
-		'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'ts', 'ч' => 'ch',
-		'ш' => 'sh', 'щ' => 'shh', 'ъ' => '', 'ы' => 'y', 'ь' => '',
-		'э' => 'e', 'ю' => 'u', 'я' => 'ya',
+        // Other translit
+        // Make shure that you set locale for using iconv
+        $s = iconv('UTF-8', 'ASCII//TRANSLIT', $s);
 
-		'-' => '-', ' ' => '-', '.' => '-', ',' => '-',
+        // Remove symbols
+        $s = preg_replace('/[^\-0-9a-z]+/i', '', $s);
 
-	];
+        // Double spaces
+        $s = preg_replace('/\-+/', '-', $s);
 
-	private static $_replace = [
-		'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd',
-		'е' => 'e', 'ё' => 'e', 'ж' => 'j', 'з' => 'z', 'и' => 'i',
-		'й' => 'y', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n',
-		'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't',
-		'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'ts', 'ч' => 'ch',
-		'ш' => 'sh', 'щ' => 'shh', 'ъ' => '', 'ы' => 'y', 'ь' => '',
-		'э' => 'e', 'ю' => 'u', 'я' => 'ya',
+        // Spaces at begin and end
+        $s = preg_replace('/^\-*(.*?)\-*$/', '$1', $s);
 
-		'-' => '-', ' ' => '-', '.' => '-', ',' => '-',
-	];
-
-	/**
-	 * Tranlit
-	 * @param string $text text for transliteration.
-	 * @return string
-	 */
-	public static function t($text)
-	{
-		$text = mb_strtolower($text);
-
-		// Cyrilic and symbols translit
-		$replace = self::$_replace;
-		$s = '';
-		for ($i = 0; $i < mb_strlen($text); $i++) { 
-			$c = mb_substr($text, $i, 1);
-			if (array_key_exists($c, $replace)) {
-				$s .= $replace[$c];
-			} else {
-				$s .= $c;
-			}
-		}
-
-		// Other translit
-		// Make shure that you set locale for using iconv
-		$s = iconv('UTF-8', 'ASCII//TRANSLIT', $s);
-
-		// Remove symbols
-		$s = preg_replace('/[^\-0-9a-z]+/i', '', $s);
-
-		// Double spaces
-		$s = preg_replace('/\-+/', '-', $s);
-
-		// Spaces at begin and end
-		$s = preg_replace('/^\-*(.*?)\-*$/', '$1', $s);
-
-		return $s;
-	}
-
+        return $s;
+    }
 }
